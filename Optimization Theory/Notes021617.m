964267688c883e529aeb79721d56d7a424bc7283
Brian McIlwain <brianwmcilwain@gmail.com>
2017-02-16 17:50:36 -0600

    Example from class. It's working!!

--- Optimization Theory/Notes021617.m
+++ Optimization Theory/Notes021617.m
@@ -5,21 +5,17 @@
 format rat;
 
 % Input matrix
-A = [1 -5 -4 0 0 0 0 0; 0 6 4 1 0 0 0 24; 0 1 2 0 1 0 0 6; 0 -1 1 0 0 1 0 1; 0 0 1 0 0 0 1 2]
-
-rref(A)
-
-% Format given matrix
-A = swapCol(A, 2, 6);
-A = swapCol(A, 3, 7);
+    I5 = eye(5);
+    A1 = [-5 -4 0; 6 4 24; 1 2 6; -1 1 1; 0 1 2];
+    % Horizontal concactnate
+    A = horzcat(I5, A1)
 
 % Division for next step
-Adiv = (A(:,8)./A(:,2))
-
-% Specify piviots for rref
-A = swapCol(A, 6, 4);
+Adiv = (A(:,8)./A(:,6))
 
 % Show
 b1 = rref(A)
 
-%A = swapCol(A, )
+% Next step
+A = swapCol(A, 2, 6);
+b2 = rref(A)
