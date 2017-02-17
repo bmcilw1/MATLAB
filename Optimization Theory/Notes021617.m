39e8b93722041409885ea46dc8e8bb7b79c05e1a
Brian McIlwain <brianwmcilwain@gmail.com>
2017-02-17 11:26:13 -0600

    Freddy miiks problem done.

--- Optimization Theory/Notes021617.m
+++ Optimization Theory/Notes021617.m
@@ -1,21 +1,22 @@
 %Brian McIlwain
 %Simplex tableau
-
-% Store as rationals
-format rat;
+% Freddy Miiks problem from text
+format rat
 
 % Input matrix
-    I5 = eye(5);
-    A1 = [-5 -4 0; 6 4 24; 1 2 6; -1 1 1; 0 1 2];
-    % Horizontal concactnate
-    A = horzcat(I5, A1)
-
-% Division for next step
-Adiv = (A(:,8)./A(:,6))
+A = horzcat(eye(5), [-5 -4 0; 6 4 24; 1 2 6; -1 1 1; 0 1 2]);
 
 % Show
-b1 = rref(A)
+A = rref(A)
+
+% Division for next step
+Adiv = A(:,length(A))./A(:,6)
 
 % Next step
 A = swapCol(A, 2, 6);
-b2 = rref(A)
+A = rref(A)
+
+Adiv = A(:,length(A))./A(:,7)
+
+A = swapCol(A, 3, 7);
+A = rref(A)
