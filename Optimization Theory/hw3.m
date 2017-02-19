e633af1b136a9bbfdade7f920ef3a73e09a98d27
Brian McIlwain <brianwmcilwain@gmail.com>
2017-02-19 00:04:32 -0600

    FInished hw3

--- Optimization Theory/hw3.m
+++ Optimization Theory/hw3.m
@@ -17,3 +17,28 @@ A = horzcat(eye(4), [1 2 -3 5 4; 5 -2 0 6 8; 2 3 -2 3 3; 1 0 1 -2 0])
 divorig = (A(:,length(A))./A(:,8))
 A = swapCol(A, 4, 8);
 A = rref(A)
+
+% Prob 8a
+A = horzcat(eye(4), [-16 -15 0; 40 31 124; -1 1 1; 1 0 3])
+divorig = (A(:,length(A))./A(:,5))
+A = swapCol(A, 4, 5);
+A = rref(A)
+
+div = (A(:,length(A))./A(:,6))
+A = swapCol(A, 2, 6);
+A = rref(A)
+
+div = (A(:,length(A))./A(:,5))
+A = swapCol(A, 3, 5);
+A = rref(A)
+
+% Prob 8b
+A = horzcat(eye(4), [-16 -15 0; 40 31 124; -1 1 1; 1 0 3])
+divorig = (A(:,length(A))./A(:,6))
+
+A = swapCol(A, 3, 6);
+A = rref(A)
+
+div = (A(:,length(A))./A(:,5))
+A = swapCol(A, 2, 5);
+A = rref(A)
