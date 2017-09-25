b03228ddb66ee29d5a7dc85dc7779747f2097928
Brian McIlwain <brianwmcilwain@gmail.com>
2017-09-25 17:16:37 -0500

    hw1 better formatting

--- NN/hw1.m
+++ NN/hw1.m
@@ -5,8 +5,8 @@
 % Assume there are R inputs and S neurons in the neural network.  
 % Verify the learning using data given below.
 
-p = [1 1 2 2 3 3 4 4 ; 4 5 4 5 1 2 1 2];
-t = [0 0 0 0 1 1 1 1];
+p = { [1 4]' [1 5]' [2 4]' [2 5]' [3 1]' [3 2]' [4 1]' [4 2]'};
+t = {0 0 0 0 1 1 1 1};
 
 
 
