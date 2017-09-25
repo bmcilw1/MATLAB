3ca47c93fa9f9413256998c0086d991835d32aa7
Brian McIlwain <brianwmcilwain@gmail.com>
2017-09-25 16:44:35 -0500

    Initial commit of hw1

---
+++ NN/hw1.m
@@ -0,0 +1,13 @@
+% Brian McIlwian
+% Hw #1 Perceptron Learning
+
+% (a) Write a program to implement perceptron learning algorithm. 
+% Assume there are R inputs and S neurons in the neural network.  
+% Verify the learning using data given below.
+
+p = [1 1 2 2 3 3 4 4 ; 4 5 4 5 1 2 1 2];
+t = [0 0 0 0 1 1 1 1];
+
+
+
+% (b) Also, verify your algorithm for any multi-neuron classification problem.
