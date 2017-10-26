f5eaf233619f91e52aeac7aba0e66cba421dbf05
Brian McIlwain <brianwmcilwain@gmail.com>
2017-10-26 15:35:15 -0500

    Improved hw3 layout for greater extensibility

--- NN/hw3.m
+++ NN/hw3.m
@@ -54,30 +54,9 @@ six =   [ 1  1  1  1  1;
 p(:,7) = six(:);
 
 % Test numbers 0-1, 0-2, ..., 0-6
-numFlips = [2, 4, 6];
-R = ones(3, 6, 2);
-for maxNum = 2:7
-    % Will go from 0 to maxNum - 1
-    % Hebb rule W: sum of all PnPn'
-    WHebb = zeros(30,30);
-    WSu = zeros(30,30);
-    for i=1:maxNum
-        WHebb = WHebb + p(:,i)*p(:,i)';
-        WSu = WSu + p(:,i)*inv(p(:,i)'*p(:,i))*p(:,i)';
-    end
-    
-    %Test network
-    for i = 1:3
-        recallHebb = test_hw3( WHebb, p, maxNum, numFlips(i));
-        recallSu = test_hw3( WSu, p, maxNum, numFlips(i));
-        R(i,maxNum-1, 1) = recallHebb/maxNum*100;
-        R(i,maxNum-1, 2) = recallSu/maxNum*100;
-        %fprintf('Hebb rule network has accuracy of %i%% for numbers 0-%i with %i noise flips.\n', H(i,maxNum-1), maxNum-1, numFlips(i));
-    end
-end
+R = run_hw3(p);
 
 I = 1:6;
-
 figure('name', 'Part I and II - Reconizing Numbers');
 
 subplot(1,3,1);
---
+++ NN/run_hw3.m
@@ -0,0 +1,24 @@
+function [ R ] = run_hw3(p)
+%test_hw3 Run tests on given network
+    numFlips = [2, 4, 6];
+    R = ones(3, 6, 2);
+    for maxNum = 2:7
+        % Will go from 0 to maxNum - 1
+        % Hebb rule W: sum of all PnPn'
+        WHebb = zeros(30,30);
+        WSu = zeros(30,30);
+        for i=1:maxNum
+            WHebb = WHebb + p(:,i)*p(:,i)';
+            WSu = WSu + p(:,i)*inv(p(:,i)'*p(:,i))*p(:,i)';
+        end
+
+        %Test network
+        for i = 1:3
+            recallHebb = test_hw3( WHebb, p, maxNum, numFlips(i));
+            recallSu = test_hw3( WSu, p, maxNum, numFlips(i));
+            R(i,maxNum-1, 1) = recallHebb/maxNum*100;
+            R(i,maxNum-1, 2) = recallSu/maxNum*100;
+            %fprintf('Hebb rule network has accuracy of %i%% for numbers 0-%i with %i noise flips.\n', H(i,maxNum-1), maxNum-1, numFlips(i));
+        end
+    end
+end
