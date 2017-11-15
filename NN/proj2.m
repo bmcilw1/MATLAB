ecaaffad8c325dda4740face740b02538814d068
Brian McIlwain <brianwmcilwain@gmail.com>
2017-11-15 13:28:48 -0600

    added additonal given code

--- NN/proj2.m
+++ NN/proj2.m
@@ -16,8 +16,21 @@ alpha = 0.12; % Given in problem
 
 v(1) = 1.2*sin(2*1*pi/3);
 a(1) = W*[v(1) 0]';
-s(1) = -2 + 4*rand();
+s(1) = -2 + 4*rand(); % Input Signal
 m = 1.2*sin(2*pi/3-3*pi/2);
 e(1) = m-a(1);
 r(1) = s(1)+ e(1);
 W = W + 2*alpha*e(1)*[v(1), 0];
+
+for k = 2:i
+    s(k) = -2 + 4*rand(); % Input Signal
+    m = 1.2*sin(2*pi*k/3-3*pi/3);
+    v(k) = 1.2*sin(2*k*pi/3);
+    a(k) = W*[v(k) v(k-1)]';
+    
+    e(k) = m-a(k); % error
+    r(k) = s(k) + m-a(k); % resistance
+    X(k) = W(1);
+    Y(k) = W(2);
+    W = W + 2*alpha*e(k)*[v(k) v(k-1)];
+end
