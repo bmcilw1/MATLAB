067234ea25eedcbd440b7ff7ec6d617d20835a5c
Brian McIlwain <brianwmcilwain@gmail.com>
2017-11-15 20:41:25 -0600

    Cleaned up given code

--- NN/proj2.m
+++ NN/proj2.m
@@ -5,34 +5,35 @@
 % LMS
 i=80; %time
 W=[0 0];
-X=zeros(1,i);
-Y=zeros(1,i);
 e=zeros(1,i);
 s=zeros(1,i);
 v=zeros(1,i);
 a=zeros(1,i);
 r=zeros(1,i);
 alpha = 0.12; % Given in problem
+e_limit = 10^-3; % Given
 
-v(1) = 1.2*sin(2*1*pi/3);
-a(1) = W*[v(1) 0]';
-s(1) = -2 + 4*rand(); % Input Signal
-m = 1.2*sin(2*pi/3-3*pi/2);
-e(1) = m-a(1);
-r(1) = s(1)+ e(1);
-W = W + 2*alpha*e(1)*[v(1), 0];
-
-for k = 2:i
+for k = 1:i
     s(k) = -2 + 4*rand(); % Input Signal
-    m = 1.2*sin(2*pi*k/3-3*pi/3);
-    v(k) = 1.2*sin(2*k*pi/3);
-    a(k) = W*[v(k) v(k-1)]';
+    m = .12*sin(2*pi*k/3+pi/2); % Actual noise
+    v(k) = 1.2*sin(2*k*pi/3); % Noise vector
+
+    % Handle special case k = 1
+    if (k == 1)
+        a(k) = W*[v(1) 0]';
+    else
+        a(k) = W*[v(k) v(k-1)]'; % Reconstructed noise to remove
+    end
     
-    e(k) = m-a(k); % error
-    r(k) = s(k) + m-a(k); % resistance
-    X(k) = W(1);
-    Y(k) = W(2);
-    W = W + 2*alpha*e(k)*[v(k) v(k-1)];
+    e(k) = m-a(k); % Error between actual noise and filter output
+    r(k) = s(k) + e(k); % Reconstructed signal
+   
+    % Handle special case k = 1
+    if (k == 1)
+        W = W + 2*alpha*e(k)*[v(k) 0]; % update weight
+    else
+        W = W + 2*alpha*e(k)*[v(k) v(k-1)]; % update weight
+    end
 end
 
 W
