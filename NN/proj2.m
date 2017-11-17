eeb6ba258d28d5ed1d349988946ba8b30f50c6cb
Brian McIlwain <brianwmcilwain@gmail.com>
2017-11-16 18:27:40 -0600

    Added code to auto grab R matrix

--- NN/proj2.m
+++ NN/proj2.m
@@ -7,10 +7,14 @@ i=80; %time
 s=zeros(1,i); % pure input signal
 v=zeros(1,i); % raw input noise near noise source
 m=zeros(1,i); % percieved noise near input signal
+v_kMinus1 = v;
 
 for k = 1:i
     s(k) = -2 + 4*rand();
     v(k) = 1.2*sin(2*pi*k/3);
+    if (k > 1)
+        v_kMinus1(k) = v(k-1);
+    end
     m(k) = .12*sin(2*pi*k/3+pi/2);
 end
 
@@ -33,6 +37,9 @@ title('Original minus restored signal');
 
 %% Countor Plot
 R=[.72 -.36; -.36 .72];
+%R=cov(v,v_kMinus1);
+%h = xcorr([(s+m) (s+m)]', [v v_kMinus1]')
+%h=[mean((s+m)'*v) mean((s+m)'*v_kMinus1)]'
 h=[0; -.06234];
 c=.0205;
 [x,y] = meshgrid(-1:.01:1,-1:.01:1);
