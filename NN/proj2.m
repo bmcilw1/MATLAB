3f2fa8221f4982a0de71920749b2ce38e3be6f0c
Michael Cole <mcole5@lsu.edu>
2017-11-17 18:06:08 -0600

    modified:   NN/proj2.m

--- NN/proj2.m
+++ NN/proj2.m
@@ -3,7 +3,7 @@
 % Project 2
 
 % LMS
-i=80000; %time
+i=80; %time
 s=zeros(1,i); % pure input signal
 v=zeros(1,i); % raw input noise near noise source
 m=zeros(1,i); % percieved noise near input signal
@@ -17,24 +17,25 @@ disp('End of Recording. Playing Back')
 play(recorder1);
 sound = getaudiodata(recorder1);
 %}
+prompt = {'Give an equation for s(k): ','Give an equation for v(k): ','Give an equation for m(k): ',};
+dlg_title = 'Input';
+num_lines = 1;
+defaultans = {'-.2 + .4*rand()','1.2*sin(2*pi*k/3)','.12*sin(2*pi*k/3+pi/2)'};
+answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
 
-SKin = input('Give an equation for s(k): ')
-VKin = input('Give an equation for v(k): ')
-MKin = input('Give an equation for m(k): ')
-
-SK = SKin;
-VK = VKin;
-
+SK = answer{1,1}
+VK = answer{2,1}
+MK = answer{3,1}
 
 for k = 1:i
-    s(k) = SK  ;
+    s(k) = eval(SK);
 %     s(k) = -.2 + .4*rand();
-    v(k) = VK;
+    v(k) = eval(VK);
 %     v(k) = 1.2*sin(2*pi*k/3);
     if (k > 1)
         v_kMinus1(k) = v(k-1);
     end
-    m(k) = MK;
+    m(k) = eval(MK);
 %     m(k) = .12*sin(2*pi*k/3+pi/2);
 end
 alpha = .12
@@ -44,7 +45,7 @@ e_limit = 10^-3; % Given TODO: how to use this??
 
 [ W, e, r, X, Y] = lms( v, s, m, alpha);
 W
-plot_lms(v, vMinus1, s, m, alpha, e, r, X, Y);
+plot_lms(v, v_kMinus1, s, m, alpha, e, r, X, Y);
 
 figure;
 i = linspace(0, 5, 44100*5);
