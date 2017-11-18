dc2835452b79de7acbf1ad6e863763b2c483bc15
Michael Cole <mcole5@lsu.edu>
2017-11-18 16:36:57 -0600

    modified:   NN/record.m

--- NN/record.m
+++ NN/record.m
@@ -1,6 +1,6 @@
-function [ recorder1 ] = record( sampleRate, bitDepth, numChannels, length )
+function [ recorder1 ] = record( sampleRate, bitDepth, length )
 %RECORD creates an audio recording 
-    recorder1 = audiorecorder(sampleRate, bitDepth, numChannels);
+    recorder1 = audiorecorder(sampleRate, bitDepth, 1);
     recordblocking(recorder1, length) % take 3 seconds of input
 ends
 
