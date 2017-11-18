6a810ab5b97ff2e3f9644cc600587cb6da6508f0
Michael Cole <mcole5@lsu.edu>
2017-11-18 16:52:19 -0600

    Adding a special commit message for Brian!!!!1 	wow!!!!!!!!111111lololololol

--- NN/record.m
+++ NN/record.m
@@ -1,6 +1,13 @@
 function [ recorder1 ] = record( sampleRate, bitDepth, length )
 %RECORD creates an audio recording 
-    recorder1 = audiorecorder(sampleRate, bitDepth, 1);
+    h = waitbar(0,'Recorder...');
+    steps = 1000;
+    for step = 1:steps
+        % computations take place here
+        waitbar(step / steps)
+    end
+    close(h) 
+    recorder1 = audiorecorder(sampleRate, bitDepth, 1, message);
     recordblocking(recorder1, length) % take 3 seconds of input
 ends
 
